187189fb6f54552e1c736db0544f9766df9846e3
HTN <96134288+htn-dev@users.noreply.github.com>
2022-02-13 18:18:09 -0500

    initial commit

---
+++ portfolio.js
@@ -0,0 +1,26 @@
+
+function subdomainRedirect(){
+    const project1 = document.getElementById('project1')
+    const project2 = document.getElementById('project2')
+    const project3 = document.getElementById('project3')
+
+    project1.addEventListener('click', ()=>{
+        if(document.url === "http://donnab.info/beautybox.donnab.info" || document.url === "donnab.info/beautybox.donnab.info"){
+            window.location.assign("beautybox.donnab.info");
+        }
+    })
+
+    project2.addEventListener('click', ()=>{
+        if(document.url === "http://donnab.info/noteboard.donnab.info" || document.url === "donnab.info/noteboard.donnab.info"){
+            window.location.assign("noteboard.donnab.info");
+        }
+    })
+
+    project3.addEventListener('click', ()=>{
+        if(document.url === "http://donnab.info/cryptopair.donnab.info" || document.url === "donnab.info/cryptopair.donnab.info"){
+            window.location.assign("cryptopair.donnab.info");
+        }
+    })
+}
+
+subdomainRedirect()
